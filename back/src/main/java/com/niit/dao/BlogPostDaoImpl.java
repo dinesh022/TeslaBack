45f1584fe70bcc1b44607ca67653f3ca54e291c3
dinesh <dinesh@Dinesh>
2018-02-28 20:45:53 +0530

    Blog Added

---
+++ back/src/main/java/com/niit/dao/BlogPostDaoImpl.java
@@ -0,0 +1,34 @@
+package com.niit.dao;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.niit.model.BlogPost;
+@Repository
+@Transactional
+public class BlogPostDaoImpl implements BlogPostDao {
+@Autowired
+private SessionFactory sessionFactory;
+	@Override
+	public void addBlogPost(BlogPost blogPost) {
+		// TODO Auto-generated method stub
+    Session session=sessionFactory.getCurrentSession();
+    session.save(blogPost);
+	}
+	@Override
+	public List<BlogPost> listOfBlogs(int approved) {
+		// TODO Auto-generated method stub
+		Session session=sessionFactory.getCurrentSession();
+		Query query=session.createQuery("from blogPost where approved="+approved);
+		List<BlogPost> blogs=query.list();
+		return blogs;
+	}
+
+}
